dd1afba96645f3fdc6d2fc0811bd73206d0861c2
Shalabh Aggarwal <shalabh7777@gmail.com>
2016-06-14 15:12:07 +0530

    Part 2: ORM Based REST API

--- my_app/__init__.py
+++ my_app/__init__.py
@@ -1,9 +1,11 @@
 from flask import Flask
 from flask.ext.sqlalchemy import SQLAlchemy
+from flask.ext.restless import APIManager
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/test.db'
 db = SQLAlchemy(app)
+manager = APIManager(app, flask_sqlalchemy_db=db)
 
 from my_app.catalog.views import catalog
 app.register_blueprint(catalog)
