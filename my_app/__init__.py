0eb4aaeec6a8edb0a550b0ce5bfa6870f92cd8c1
Shalabh Aggarwal <shalabh7777@gmail.com>
2016-06-15 00:34:17 +0530

    ORM Independent REST API

--- my_app/__init__.py
+++ my_app/__init__.py
@@ -1,11 +1,11 @@
 from flask import Flask
 from flask.ext.sqlalchemy import SQLAlchemy
-from flask.ext.restless import APIManager
+from flask.ext.restful import Api
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/test.db'
 db = SQLAlchemy(app)
-manager = APIManager(app, flask_sqlalchemy_db=db)
+api = Api(app)
 
 from my_app.catalog.views import catalog
 app.register_blueprint(catalog)
--- my_app/catalog/views.py
+++ my_app/catalog/views.py
@@ -1,8 +1,17 @@
-from flask import Blueprint
-from my_app import manager
+import json
+from flask import Blueprint, abort
+from flask.ext.restful import Resource
+from flask.ext.restful import reqparse
 from my_app.catalog.models import Product
+from my_app import api, db
+
 
 catalog = Blueprint('catalog', __name__)
+
+parser = reqparse.RequestParser()
+parser.add_argument('name', type=str)
+parser.add_argument('price', type=float)
+
 
 @catalog.route('/')
 @catalog.route('/home')
@@ -10,4 +19,40 @@ def home():
     return "Welcome to the Catalog Home."
 
 
-manager.create_api(Product, methods=['GET', 'POST'])
+class ProductApi(Resource):
+
+    def get(self, id=None, page=1):
+        if not id:
+            products = Product.query.paginate(page, 10).items
+        else:
+            products = [Product.query.get(id)]
+        if not products:
+            abort(404)
+        res = {}
+        for product in products:
+            res[product.id] = {
+                'name': product.name,
+                'price': product.price,
+            }
+        return json.dumps(res)
+
+    def post(self):
+        args = parser.parse_args()
+        name = args['name']
+        price = args['price']
+        product = Product(name, price)
+        db.session.add(product)
+        db.session.commit()
+        res = {}
+        res[product.id] = {
+            'name': product.name,
+            'price': product.price,
+        }
+        return json.dumps(res)
+
+api.add_resource(
+   ProductApi,
+   '/api/product',
+   '/api/product/<int:id>',
+   '/api/product/<int:id>/<int:page>'
+)
